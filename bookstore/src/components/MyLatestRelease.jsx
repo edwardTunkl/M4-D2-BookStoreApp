2a7371862d1dff187f719fd65861723ba741e996
edwardTunkl <eddytunkl@googlemail.com>
2021-08-24 16:56:13 +0200

    MyLatestRelease added

--- bookstore/src/components/MyLatestRelease.jsx
+++ bookstore/src/components/MyLatestRelease.jsx
@@ -13,17 +13,19 @@ render() {
         <Container>
         <Row>
           <Col>
-              <Card>
-              <Card.Img variant="top" src="holder.js/100px180" />
+          {
+            books.map(book => (
+
+              <Card key={book.asin} >
+              <Card.Img variant="top" src={book.img} />
               <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up the bulk of
-                  the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
+                <Card.Title>{book.title}</Card.Title>
+                <Button variant="primary">Buy</Button>
               </Card.Body>
               </Card>
+            )
+          )
+          }
           </Col>
         </Row>
         </Container>
