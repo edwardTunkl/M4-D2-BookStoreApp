a05078e36d05abfac5b805093756b7b48e495158
edwardTunkl <eddytunkl@googlemail.com>
2021-08-24 16:48:45 +0200

    Navbar, Footer, Jumbotron added (works), Cards added (not fixed yet)

--- bookstore/src/App.js
+++ bookstore/src/App.js
@@ -3,12 +3,14 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import MyNav from './components/MyNav'
 import MyJumbotron from './components/MyJumbotron';
 import MyFooter from './components/MyFooter';
+import MyLatestRelease from './components/MyLatestRelease';
 
 function App() {
   return (
     <div>
       <MyNav name="myBookStore" />
       <MyJumbotron name = "myBookStore" />
+      <MyLatestRelease />
       <MyFooter />
     </div>
   );
