8d80126e6441c3c357d6a45e29b2be84456b9c1a
edwardTunkl <eddytunkl@googlemail.com>
2021-09-02 15:14:32 +0200

    creating Rout/er components

--- bookstore/src/App.js
+++ bookstore/src/App.js
@@ -6,9 +6,13 @@ import MyFooter from "./components/MyFooter";
 import MyBookList from "./components/MyBookList";
 import MySingleBook from "./components/MySingleBook";
 import CommentArea from "./components/CommentArea";
+import About from "./components/About";
+import Register from "./components/Register";
+
 import books from "./data/fantasy.json";
 import { Container, Row, Col } from "react-bootstrap";
 import { Component } from "react";
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 
 class App extends Component {
   state = {
@@ -24,20 +28,24 @@ class App extends Component {
   render() {
     return (
       <div style={{ backgroundColor: "lightGrey" }}>
-        <MyNav name="myBookStore" />
-        <MyJumbotron name="myBookStore" />
-        <Container style={{ width: "300px" }}>
-          <MySingleBook book={books[5]} selectMovie={this.selectMovie} />
-        </Container>
-        <Row className="mx-3 my-2">
-          <Col sm={8}>
-            <MyBookList books={books} selectMovie={this.selectMovie} />
-          </Col>
-          <Col sm={4}>
-            <CommentArea asin={this.state.selectedAsin} />
-          </Col>
-        </Row>
-        <MyFooter />
+        <Router>
+            <MyNav name="myBookStore" />
+            <MyJumbotron name="myBookStore" />
+          <Rout component={About} path="/about" />
+          <Rout component={Register} path="/register" />  
+            <Container style={{ width: "300px" }}>
+              <MySingleBook book={books[5]} selectMovie={this.selectMovie} />
+            </Container>
+            <Row className="mx-3 my-2">
+              <Col sm={8}>
+                <MyBookList books={books} selectMovie={this.selectMovie} />
+              </Col>
+              <Col sm={4}>
+                <CommentArea asin={this.state.selectedAsin} />
+              </Col>
+            </Row>
+            <MyFooter />
+        </Router>
       </div>
     );
   }
--- bookstore/src/components/MyNav.jsx
+++ bookstore/src/components/MyNav.jsx
@@ -1,19 +1,32 @@
 import {Container, Navbar, Nav} from 'react-bootstrap'
+import { Link,withRouter } from 'react-router-dom'
 
-const MyNav = ({name}) => {
+const MyNav = ({name}, {location}) => {
 
 return (
     <Container>
         <Navbar bg="dark" variant="dark">
         <Navbar.Brand href="#home">{name}</Navbar.Brand>
         <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#about">About</Nav.Link>
-          <Nav.Link href="#browse">Browse</Nav.Link>
+            <Link to="/">
+                <div 
+                className={location.pathname === '/' ? 'nav-link active' : 'nav-link'}
+                >Home</div>
+            </Link>
+            <Link to="/about">
+                <div 
+                className={location.pathname === '/about' ? 'nav-link active' : 'nav-link'}
+                >About</div>
+            </Link>
+            <Link to="/about">
+                <div 
+                className={location.pathname === '/register' ? 'nav-link active' : 'nav-link'}
+                >Register</div>
+            </Link>
         </Nav>
       </Navbar>
     </Container> 
   )
 }
 
-export default MyNav
+export default withRouter(MyNav)
